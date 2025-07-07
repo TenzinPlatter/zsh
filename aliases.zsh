c68b0b6283b7742608b36a926873b586d0467694
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-07-07 16:34:28 +1000

    Auto-commit: modified 1 file

--- aliases.zsh
+++ aliases.zsh
@@ -11,7 +11,7 @@ alias cbuilds="colcon build --symlink-install --cmake-args -DCMAKE_EXPORT_COMPIL
 
 alias srz="source ~/.zshrc"
 
-alias ssr="ssh rock@rock-5b"
+alias ssr="ssh rock@rock-5b-osc4ki0g"
 # alias ssr="ssh rock@10.71.11.117"
 
 alias killpgad="kill $(pidof /home/tenzin/.scripts/open_pgadmin.sh)"
