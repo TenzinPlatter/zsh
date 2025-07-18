ed267cc67b7ac71a7c757aa4cba8fc4365e16242
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-07-18 10:59:25 +1000

    Auto-commit: modified 1 file

---
+++ node_bloat.zsh
@@ -0,0 +1,31 @@
+node() {
+  if ! command -v npm > /dev/null; then
+    export NVM_DIR="$HOME/.config/nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+  fi
+
+  node "$@"
+}
+
+npm() {
+  if ! command -v npm > /dev/null; then
+    export NVM_DIR="$HOME/.config/nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+  fi
+
+  npm "$@"
+}
+
+pnpm() {
+  if ! command -v pnpm > /dev/null; then
+    export PNPM_HOME="/home/tenzin/.local/share/pnpm"
+    case ":$PATH:" in
+      *":$PNPM_HOME:"*) ;;
+      *) export PATH="$PNPM_HOME:$PATH" ;;
+    esac
+  fi
+  
+  pnpm "$@"
+}
