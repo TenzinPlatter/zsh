487789566f38875340afc17c154de5e38fe976a8
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-08-20 23:25:14 +1000

    Save current state before converting to submodule

---
+++ .zsh_plugins.zsh
@@ -0,0 +1,9 @@
+fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions" )
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting" )
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-MichaelAquilina-SLASH-zsh-you-should-use" )
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-MichaelAquilina-SLASH-zsh-you-should-use/you-should-use.plugin.zsh"
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-MichaelAquilina-SLASH-zsh-you-should-use/zsh-you-should-use.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-fdellwing-SLASH-zsh-bat" )
+source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-fdellwing-SLASH-zsh-bat/zsh-bat.plugin.zsh"
--- user/aliases.zsh
+++ user/aliases.zsh
@@ -5,7 +5,8 @@ alias dk="docker"
 alias cbuild="colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
 alias cbuilds="colcon build --symlink-install --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
 
-alias srz="source ~/.zshrc"
+alias srz="source $ZDOTDIR/.zshrc"
+alias new="exec zsh"
 
 alias ssr="ssh rock@rock-5b"
 alias ssb="ssh gr@blueboat"
@@ -41,6 +42,7 @@ alias ct="cargo test"
 
 alias db='distrobox'
 alias dbr="distrobox enter ros"
+alias dbs="distrobox enter siri"
 
 alias tmuxkill="tmux kill-session"
 alias srtx="tmux source ~/.tmux.conf"
@@ -65,7 +67,7 @@ alias pca="pre-commit run --all"
 alias nv="nvim"
 alias vn="nv"
 alias nvfd='nv $(fzfd)'
-alias nvsh='nv ~/.zshrc && source ~/.zshrc'
+alias nvsh='(cd ~/.config/zsh && nv) && new'
 alias nvenv='nv ~/.zshenv && source ~/.zshenv'
 alias nvhl='nv ~/.config/hypr/.'
 alias nvcf='nv ~/.config/nvim/.'
